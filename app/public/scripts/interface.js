9c0d49892080dfacf4073031285ba3acb856b24b
Alex <aleksandernasr@gmail.com>
2015-07-13 18:47:24 +0100

    LIKES working

--- app/public/scripts/interface.js
+++ app/public/scripts/interface.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
 
 
 	$(".buttons").click(function(event){
-		console.log(event.target.id)
+		// console.log(event.target.id)
 		$.post( "/likes", { peep_id: event.target.id  })
 	})
 
